611f0ce130b574bd55e57b469fae5881d1369f06
Denis Reznikov <red_sky@mail.ru>
2023-05-24 18:15:35 +0300

    initialize

---
+++ main.py
@@ -0,0 +1,18 @@
+from machine import Pin, SoftI2C
+from vl53l1x import VL53L1X
+import time
+
+led = Pin(2,Pin.Out)
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
+distance = VL53L1X(i2c)
+led = Pin(2, Pin.OUT)
+
+def blink(time, count):
+
+
+while True:
+    if distance.read()<500:
+        led.value(1)
+    else:
+        led.value(0)
+    time.sleep_ms(200)
