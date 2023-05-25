03dc9ca95e5b2a7e1b468d0e4ebeafa48b4b7331
Denis Reznikov <red_sky@mail.ru>
2023-05-25 10:24:44 +0300

    add PWM Led blink

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from machine import Pin, SoftI2C
+from machine import Pin, SoftI2C, PWM
 from vl53l1x import VL53L1X
 import time
 
-led = Pin(2,Pin.Out)
+LED_PIN = 2
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 distance = VL53L1X(i2c)
-led = Pin(2, Pin.OUT)
-
-def blink(time, count):
-
+led = PWM(Pin(LED_PIN), freq=1)
 
 while True:
-    if distance.read()<500:
-        led.value(1)
-    else:
-        led.value(0)
-    time.sleep_ms(200)
+  dist = distance.read()
+  if dist < 500 and dist > 1:
+    freq = int((500/dist)**2)
+    print(freq)
+    freq = min(max(freq, 1), 40)
+    led.freq(freq)
+    led.duty(512)
+  else:
+    led.duty(0)
+  print(f'{dist} mm')
+  time.sleep_ms(200)
+  
