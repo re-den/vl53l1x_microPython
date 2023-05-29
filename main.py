66786b0c70bafab39934ebed5e27c851b6d5c7bd
Denis Reznikov <red_sky@mail.ru>
2023-05-29 20:20:14 +0300

    add change PWM duty
    Добавлено изменение ширины импульса и частоты в зависимости от расстояния до объекта

--- main.py
+++ main.py
@@ -5,27 +5,37 @@ import time
 LED_PIN = 2
 OLD_DIST = 0
 freq = 0
-start_range = 1000
-stop_range = start_range * 0.1
+start_range = 2000
+stop_range = int(start_range * 0.05)
 
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 distance = VL53L1X(i2c)
-led = PWM(Pin(LED_PIN), freq=10000)
+led = PWM(Pin(LED_PIN), freq=1000)
+
+OldMax = stop_range
+OldMin = start_range
+NewMax = 1023
+NewMin = 200
+
+OldRange = (OldMax - OldMin)  
+NewRange = (NewMax - NewMin)  
 
 while True:
   dist = distance.read()
+
+  
   if dist < start_range and dist > stop_range:
     freq = int((start_range/dist)**3)
-    freq = min(max(freq, 0), int(start_range))
+    freq = min(max(freq, 0), 50)
+    pwm = int((((dist - OldMin) * NewRange) / OldRange) + NewMin)
     #if dist - OLD_DIST > 5:
-    print(f'{dist} mm\t|\t{freq} Hz')
+    print(f'{dist} mm  |  {freq} Hz  |  {pwm}')
     led.freq(freq)
-    led.duty(512)
+    led.duty(pwm)
   else:
     led.duty(0)
-    if dist - OLD_DIST > 10:
-      print(f'{dist}')
+    #if dist - OLD_DIST > 10:
+    print(f'{dist}')
   OLD_DIST = dist
   freq = 0
-  time.sleep_ms(5)
-  
+  time.sleep_ms(100)
